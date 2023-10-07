3f8dfe18beee1fe90311957dc779ed70fb3ba316
m1rile <marile-lin@hotmail.fr>
2023-10-07 11:03:13 +0200

    add new page for credit limits submission

---
+++ pages/submit-credit-limits.tsx
@@ -0,0 +1,11 @@
+export default function SubmitCreditLimits() {
+    return (
+        <main>
+            <div>
+                <h1>SUBMIT CREDIT LIMITS</h1>
+
+                
+            </div>
+        </main>
+    )
+}
