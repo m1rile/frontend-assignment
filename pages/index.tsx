ec24d9dcba9a2a887f0e5de07349862630f23157
m1rile <marile-lin@hotmail.fr>
2023-10-07 11:04:09 +0200

    add in home page redirection to submission page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
+import CustomButton from '@/components/CustomButton';
 import { CreditLimit } from '@/lib/db';
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [creditLimits, setCreditLimits] = useState<CreditLimit[]>([]);
+  const router = useRouter();
+
+  const handleNavigateToSubmitPage = () => {
+    // Use router.push to navigate to the "Submit Credit Limits" page
+    router.push('/submit-credit-limits');
+  };
 
   useEffect(() => {
+    // TODO: check if there's error
     fetch('/api/credit-limits')
       .then((response) => response.json())
       .then((data) => setCreditLimits(data));
@@ -12,9 +21,19 @@ export default function Home() {
 
   return (
     <div className="p-10 xl:max-w-[80%] m-auto">
-      <h1 className="text-xl border-b-2 border-slate-400 py-2 mb-5">
+
+    <div className='flex justify-between border-b-2 border-slate-400 mb-5'>
+      <h1 className="text-xl py-2">
         Credit limits
       </h1>
+
+      <CustomButton 
+        title="Submit" 
+        containerStyles="border-2 bg-blue-400 rounded-md p-2 text-white"
+        handleClick={handleNavigateToSubmitPage}
+      />
+    </div>
+
       <ul>
         {creditLimits.map((creditLimit) => (
           <li key={creditLimit.id}>
