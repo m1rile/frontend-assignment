1219d038b707cac7b8708d1d68307905a52edd95
m1rile <marile-lin@hotmail.fr>
2023-10-07 12:26:43 +0200

    add & wip submission page for creation a new request

--- pages/submit-credit-limits.tsx
+++ pages/submit-credit-limits.tsx
@@ -1,10 +1,12 @@
+import SubmissionForm from "@/components/SubmissionForm";
+
 export default function SubmitCreditLimits() {
     return (
         <main>
-            <div>
-                <h1>SUBMIT CREDIT LIMITS</h1>
+            <div className="p-10 w-full text-center">
+                <h1 className="text-xl pt-2 pb-4">SUBMIT CREDIT LIMITS</h1>
 
-                
+                <SubmissionForm />
             </div>
         </main>
     )
